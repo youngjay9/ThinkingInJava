58f8dd91359b9ea15125427a39731ea4b34cd32c
jay <youngjay9@gmail.com>
2019-12-01 21:39:11 +0800

    thread

--- src/main/java/thread/App.java
+++ src/main/java/thread/App.java
@@ -7,13 +7,26 @@ import thread.producer.Producer;
 import thread.stack.MyStack;
 
 public class App {
+
   public static void main(String[] args) {
     MyStack myStack = new MyStack();
     Producer producer = new Producer(myStack);
     Consumer consumer = new Consumer(myStack);
     ProducerThread p_thread = new ProducerThread(producer);
+    p_thread.setName("producerThread1");
     ConsumerThread c_thread = new ConsumerThread(consumer);
+    c_thread.setName("consumerThread1");
     p_thread.start();
     c_thread.start();
+
+    MyStack myStack2 = new MyStack();
+    Producer producer2 = new Producer(myStack2);
+    Consumer consumer2 = new Consumer(myStack2);
+    ProducerThread p_thread2 = new ProducerThread(producer2);
+    p_thread2.setName("producerThread2");
+    ConsumerThread c_thread2 = new ConsumerThread(consumer2);
+    c_thread2.setName("consumerThread2");
+    p_thread2.start();
+    c_thread2.start();
   }
 }
--- src/main/java/thread/stack/MyStack.java
+++ src/main/java/thread/stack/MyStack.java
@@ -13,35 +13,33 @@ public class MyStack {
 
   synchronized public void push() {
     try {
-      while (list.size() > 0) {
-        logger.info(Thread.currentThread().getName()+" begin to wait " + System.currentTimeMillis());
+      while (list.size() == 1) {
         this.wait(); // thread 從 blocked queue 中再回來時，從下面一行開始執行
-        logger.info(Thread.currentThread().getName()+" end wait " + System.currentTimeMillis());
       }/* 當有多個 thread 執行時，需用 while loop 檢查 list 條件(即時更新 list 資料)，檢核該 thread 是否繼續 wait ....   */
 
       /* 這一段模擬產生資料 */
-      for(int i=0; i<100; i++){
-        list.add(String.valueOf(i));
-      }
+      list.add("F");
 
       this.notifyAll();// 當有多個 thread 執行時，需用 notifyall()，避免只喚醒都是執行 push 的 thread，造成所有 thread 都在 wait...
+
+      logger.info("push=" + list.size());
 
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
   }
 
-  synchronized public void pop(){
+  synchronized public void pop() {
     try {
-      while(list.size() == 0){
-        logger.info(Thread.currentThread().getName()+" begin to wait " + System.currentTimeMillis());
+      while (list.size() == 0) {
+        logger.info(
+            Thread.currentThread().getName() + " begin to wait ");
         this.wait(); // thread 從 blocked queue 中再回來時，從下面一行開始執行
-        logger.info(Thread.currentThread().getName()+" end wait " + System.currentTimeMillis());
       }
 
       /* 這一段模擬取資料 */
-      for(int i=0; i<list.size();i++){
-        logger.info("pop: "+list.remove(i));
+      for (int i = 0; i < list.size(); i++) {
+        logger.info("pop: " + list.remove(i));
       }
 
       this.notifyAll();// 當有多個 thread 執行時，需用 notifyall()，避免只喚醒都是執行 pop 的 thread，造成所有 thread 都在 wait...
